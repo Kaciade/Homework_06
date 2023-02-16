38d116decc79902b10924e173e9612294d1a78f3
Kaciade <valerypolyakov60@gmail.com>
2023-02-16 19:54:22 +0400

    задание 1 исправлено

--- Tasks/Task_01/Program.cs
+++ Tasks/Task_01/Program.cs
@@ -4,21 +4,18 @@ Console.Write("Введите количество строк двумерног
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите количество столбцов двумерного массива: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Введите левую часть диапазона массива:          ");
-int min = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Введите правую часть диапазона массива:         ");
-int max = int.Parse(Console.ReadLine() ?? "0");
-int[,] matrix = new int [m, n];
+double max = 100;
+double[,] matrix = new double [m, n];
 Random random = new Random();
 
 Console.WriteLine();
-Console.WriteLine($"Двумерный массив размером {m} x {n}:");
+Console.WriteLine($"Двумерный массив размером {m} x {n}, заполненный вещественными числами:");
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i,j] = random.Next(min, max);
-        Console.Write($"{matrix[i,j]}\t");
+        matrix[i,j] = random.NextDouble() * max;
+        Console.Write("{0,6:F2}\t", matrix[i, j]);
     }
     Console.WriteLine();
 }
